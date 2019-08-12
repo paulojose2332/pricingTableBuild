39f6c43a889c8744aec517b75e124cf25097a4af
paulojose2332 <paulo.jose.n@outlook.com>
2019-08-12 09:41:09 -0300

    Add files via upload

---
+++ build/precache-manifest.df888a637ccb043d9f446c6da63e8468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5053c3bae1ea9545459e82b40b2ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf0b89e82f12156d425",
+    "url": "/static/css/2.dc33ae85.chunk.css"
+  },
+  {
+    "revision": "5ca2dae8f53fe564eed2",
+    "url": "/static/css/main.56e60a26.chunk.css"
+  },
+  {
+    "revision": "faf0b89e82f12156d425",
+    "url": "/static/js/2.30d8659e.chunk.js"
+  },
+  {
+    "revision": "5ca2dae8f53fe564eed2",
+    "url": "/static/js/main.3075912c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
